a350c8e5517b9330c9bebe36540716fc6397cace
Lopez <ialopez.contacto@gmail.com>
2025-04-27 20:30:17 +0200

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -89,7 +89,7 @@ export async function POST(request: Request) {
         await sendFrameNotification({
           fid,
           title: `Welcome to ${appName}`,
-          body: `All kinds of onchain payments made easy, on Warpcast. You can start making your own 100% FREE payment links!`,
+          body: `Make onchain payments simple and seamless with this Warpcast/Coinbase Miniapp, or directly from the browser. Start creating your own 100% FREE payment links today!`,
         });
       } else {
         await deleteUserNotificationDetails(fid);
