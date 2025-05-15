35c652dbdaf5543ac9111939cf92dcac37206da4
Lopez <ialopez.contacto@gmail.com>
2025-05-15 23:25:35 +0200

    Update WarpPayHome.tsx

--- app/components/WarpPayHome.tsx
+++ app/components/WarpPayHome.tsx
@@ -30,7 +30,7 @@ const WarpPayHome: React.FC<WarpPayHomeProps> = ({ onAction }) => {
         label: "Earn",
         desc: "Excute payments before the automatic execution, get 1%",
         action: "earn",
-        enabled: true,
+        enabled: false,
         fee: 0,
       },
       {
